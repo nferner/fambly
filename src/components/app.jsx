68d19a55d3c8f26d7dcdaecdf366f919711451ba
Neil Ferner <n.e.ferner@gmail.com>
2018-07-22 17:54:11 -0400

    new structure created

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+  render () {
+    return (
+      <div class='appWrap'>
+        <p class='appP'>This Application is connected</p>
+      </div>
+    )
+  }
+}
+
+export default App
